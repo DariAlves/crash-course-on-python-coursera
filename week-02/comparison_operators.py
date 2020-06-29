109065a68daee6bc1e444fe314251d7263d34e30
Dari Alves <darieltondf@gmail.com>
2020-06-28 22:13:03 -0300

    comparison operators

---
+++ week-02/comparison_operators.py
@@ -0,0 +1,19 @@
+# Greater than
+print(10 > 1)
+
+# Lesser than
+print(1 < 2)
+
+# Equal operator
+print("cat" == "dog")
+
+# Not equals operator
+print(1 != 2)
+
+# Logical operators
+# and, or, not
+
+print(1 > 2 and 5 > 4)
+print(1 > 2 or 5 > 4)
+print(42 == "answer") # False
+print(not 42 == "answer") # True
