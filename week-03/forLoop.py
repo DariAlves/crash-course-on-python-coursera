006166b3d297d11dd8acfa82343dee291a5221a5
Dari Alves <darieltondf@gmail.com>
2020-07-17 22:09:17 -0300

    for loop

---
+++ week-03/forLoop.py
@@ -0,0 +1,29 @@
+# 1
+for x in range(5):
+    print(x)
+
+# 2 
+def square(n):
+    return n*n
+
+def sum_squares(x):
+    sum = 0
+    for n in range(x):
+        sum += square(n)
+    return sum
+
+print(sum_squares(10)) # Should be 285
+
+# 3
+friends = ['Iori', 'Kyo', 'Terry', 'Joe']
+for friend in friends:
+    print("Hi, " + friend)
+
+# 4
+values = [23, 52, 59, 37, 48]
+sum = 0
+length = 0
+for value in values:
+    sum += value
+    length += 1
+print("Total sum: " + str(sum) + " - Average: " + str(sum/length))
